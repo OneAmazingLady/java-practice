72c9ed635a8936d935d6675826dfc38b0a6ccdf3
Nakia's Peace of Life <NPOL@Nakiass-MBP.fios-router.home>
2017-06-04 17:38:02 -0400

    adding Hello World Practice class

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/*Hello World project
+* Create simple class and print Hello World to the console
+* author: Nakia Porter on 6/4/17
+*/
+
+
+
+// No imports needed for this example
+
+
+
+public class HelloWorld{//public class declaration of the HelloWorld Class
+
+	public static void main(String args[]){//this method will take in any 
+	//arguments from the command line as a String array field labled args
+	System.out.println("Hello World");}
+	
+}
+
